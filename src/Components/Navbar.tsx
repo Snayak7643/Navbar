21484726cacb3116f6197639899f59816361b782
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-08 19:55:53 +0530

    styling navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,11 +3,38 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src="" className="logo" alt="logo" />
+          <button className="nav-toggle">Button</button>
+        </div>
+        <div className="links-container">
+          <ul className="links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.google.com">Fb</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Insta</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
