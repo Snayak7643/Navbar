055a8c111f23bb9eaf7936c497bbac5e80c88ddb
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-09 12:38:24 +0530

    Conditional render added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,48 +3,45 @@ import { Link } from "react-router-dom";
 import { IoLogoDesignernews, IoIosList } from "react-icons/io";
 
 const Navbar = () => {
-  const handleClick = () => {
-    if (show === "social-icons") setShow("show-icons");
-    else {
-      setShow("social-icons");
-    }
-  };
-
-  const [show, setShow] = useState("social-icons");
+  const [show, setShow] = useState<boolean>(true);
 
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <IoLogoDesignernews className="logo" />
-          <button className="nav-toggle" onClick={handleClick}>
+          <button className="nav-toggle" onClick={() => setShow(!show)}>
             <IoIosList />
           </button>
         </div>
-        <div className="link-container">
-          <ul className="links">
+        {show === true && (
+          <div className="link-container">
+            <ul className="links">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        )}
+        {show === true && (
+          <ul className="social-icons">
             <li>
-              <Link to="/">Home</Link>
+              <a href="https://www.google.com">Fb</a>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <a href="https://www.google.com">Insta</a>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <a href="https://www.google.com">Twitter</a>
             </li>
           </ul>
-        </div>
-        <ul className={show}>
-          <li>
-            <a href="https://www.google.com">Fb</a>
-          </li>
-          <li>
-            <a href="https://www.google.com">Insta</a>
-          </li>
-          <li>
-            <a href="https://www.google.com">Twitter</a>
-          </li>
-        </ul>
+        )}
       </div>
     </nav>
   );
